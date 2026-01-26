35d3e51584abeb69d4f0806cf584f837df9b42a5
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-27 00:44:37 +0530

    variable declare

--- first.js
+++ first.js
@@ -1,1 +1,24 @@
-console.log("i'm back again")
+let fullName = "kamal";
+console.log(fullName);
+
+let roll = 34;
+console.log(String(roll));
+
+
+let age = "33";
+console.log(parseInt(age));// purno string value ke jodi ami sonkkha te transfer korte chai tahole ami parseInt babohar korbo;
+
+let cgpa = "4.59";
+console.log(parseFloat(cgpa)); // dosomik string value ke sonkhate rupantor korte gele amake parseFloat babohar korte hobe;
+
+
+let floatSum = 0.1 + 0.2;
+console.log(floatSum);
+console.log(floatSum.toFixed(2));// dosomik sonkkhar last joto digit chai toto likhte parbo .to Fixed(1 && 2 joto dorkar) ata diye;
+
+
+y = null;
+console.log(y); // null mane holo tar vetore kichu achee jane, kintu tar vetore khali ache;
+
+x = undefined;
+console.log(x);  // undefined  mane holo tar vetore ki ache se jane na ;
