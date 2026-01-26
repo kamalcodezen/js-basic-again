d9e4b31965b17c514cd3bc05fb02e2b47364604b
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-27 01:49:16 +0530

    primitive datatype declare and practice

---
+++ jS-practice/datatype.js
@@ -0,0 +1,32 @@
+/* *********************************
+    JS DATA TYPE OF TWO  DATATYPE 
+
+    1. PRIMITIVE DATATYPE (7 DATATYPE);
+    2. NON PRIMITIVE DATATYPE(OBJECT(2 DATATYPE ARRAY, FUNCTION));
+
+***********************************/
+
+/******************************
+   1.PRIMITIVE DATATYPE---- STRING,BOOLEAN,NUMBER,NULL,UNDEFINED, BIGINT,SYMBOL;
+ *******************************/
+
+let fullName = "kamaluddin"; // string DATATYPE
+console.log(typeof fullName, fullName);
+
+let roll = 25; // number datatype
+console.log(typeof roll, roll);
+
+let isFollow = true; // boolean datatype
+console.log(typeof isFollow, isFollow);
+
+let x = null; // object but null hisabe dhora hoi datatype;
+console.log(typeof x, x);
+
+let y = undefined; // undefined datatype
+console.log(typeof y, y);
+
+let num = BigInt("123"); // BigInt datatype
+console.log(typeof num, num);
+
+let s = Symbol("hello!");// symbol datatype
+console.log(typeof s, s);
