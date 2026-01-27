135ca29dc936cd1a08c27c80c024e8da2cb37816
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 00:17:21 +0530

    arothmetic operator practice

---
+++ jS-practice/operator.js
@@ -0,0 +1,9 @@
+//  arithmetic operator 
+let a = 5;
+let b = 6;
+
+console.log('a =', a, 'b =', b);
+console.log("a + b =", a + b);
+console.log("a -  b =", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b =", a / b);
