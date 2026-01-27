76f5ff20720d41f425508229f30e7ae2638e18c7
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 01:16:47 +0530

    assignment operator practice

--- jS-practice/operator.js
+++ jS-practice/operator.js
@@ -15,5 +15,28 @@ let x = 6;
 let y = 3;
 console.log("x = ", x, "y =", y);
 // x = x + 1;
-x++;
+// x++;
 console.log("x =", x);
+
+
+console.log("y++ =", y++);    // post increment and pre decrement
+console.log("y =", y);
+
+
+/********************************************
+ *   assignment operator  += , -=, *=, **=, /=, %=
+ ********************************************/
+
+let c = 4;
+let d = 3;
+
+// c = c + 2;
+// c += 4;
+// console.log("c =", c);
+
+c -= 3;
+console.log("c =", c);
+
+d **= 4;
+console.log("d =", d);
+
