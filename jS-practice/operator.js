7f4cdb09a7272bd22963492d7bde8376bb574db7
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 02:58:16 +0530

    else if statement check

--- jS-practice/operator.js
+++ jS-practice/operator.js
@@ -76,23 +76,63 @@ console.log("cond1 || cond2 =", e < f || f === e);
  ***********************************/
 // if statement check   
 
-let age = 20;
-if (age <= 20) {
-    console.log("biye done");
-};
-if (age > 16) {
-    console.log("biye cancel");
-};
+// let age = 20;
+// if (age <= 20) {
+//     console.log("biye done");
+// };
+// if (age > 16) {
+//     console.log("biye cancel");
+// };
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+// if (mode === "light") {
+//     color = "white"
+// }
+// console.log(color);
+
+
+// if else statement check 
 
 let mode = "light";
 let color;
-
 if (mode === "dark") {
     color = "black";
-}
-if (mode === "light") {
-    color = "white"
+} else {
+    color = "white";
 }
 console.log(color);
 
 
+let age = 25;
+if (age >= 18) {
+    console.log("you can vote");
+} else {
+    console.log("you can't vote");
+};
+
+
+// even odd number
+let num = 20;
+if (num % 2 !== 0) {
+    console.log(num, "this number even");
+} else {
+    console.log(num, "this number is ODD");
+}
+
+
+// else if statement check
+let name = "zubair";
+let title;
+if (name === "kamal") {
+    title = "uddin";
+} else if (name === "aisha") {
+    title = "shikh";
+} else {
+    title = "aisha - kamal";
+}
+console.log(title);
