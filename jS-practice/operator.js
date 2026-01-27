521a15fb606927dbc96d00feb826f12b128e84b7
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 02:23:54 +0530

    if statement check practice

--- jS-practice/operator.js
+++ jS-practice/operator.js
@@ -40,3 +40,59 @@ console.log("c =", c);
 d **= 4;
 console.log("d =", d);
 
+
+
+/**************************************
+ * comparison operator
+ ***************************************/
+let s = 5;
+let v = 5;
+// console.log("2 == 5", a == b); // false
+// console.log(" 5 === 5 ", s === v);
+// console.log(" 5 !== 5 ", s !== v);
+
+console.log("5 <= 5 =", s <= v);
+
+
+
+/****************************************
+ *  logical operator
+ ****************************************/
+//  && operator
+let w = 5;
+let z = 3;
+console.log(" cond1 && cond2 =", w === 5 && 5 > 3);
+
+// || operator 
+
+let e = 4;
+let f = 7;
+
+console.log("cond1 || cond2 =", e < f || f === e);
+
+
+/********************************
+ *  condition statement
+ ***********************************/
+// if statement check   
+
+let age = 20;
+if (age <= 20) {
+    console.log("biye done");
+};
+if (age > 16) {
+    console.log("biye cancel");
+};
+
+let mode = "light";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+if (mode === "light") {
+    color = "white"
+}
+console.log(color);
+
+
