5f5411a99422f4508c81669129620ea122cc5027
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 14:39:00 +0530

    if

---
+++ jS-practice/comparison.js
@@ -0,0 +1,33 @@
+console.log(5 < 10);
+console.log(5 > 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(15 >= 10);
+console.log(5 >= 10);
+console.log(5 <= 10);
+console.log(10 <= 10);
+console.log(10 <= 100);
+
+console.log(10 == 10);
+console.log(10 == 10.1);
+
+
+//===================================
+let orange = 40;
+let apple = 50;
+console.log(orange < apple);
+console.log(orange > apple);
+console.log(orange == apple);
+
+
+
+
+/*-------------
+* ==
+* ===
+----------- */
+console.log(10 == "10");
+console.log(10 === "10");
+ 
+console.log( 10 != "10");
+console.log(10 !== "10");
---
+++ jS-practice/if .js
@@ -0,0 +1,16 @@
+/* ****************************** 
+ if (condition){
+    -- code be executed
+   }else{
+    execute code if the condition is false
+    }
+******************************/
+
+let weight = 15;
+if (weight <= 18){
+    console.log("i will carry you");
+} else{
+    console.log("i will not carry you");
+}
+
+
