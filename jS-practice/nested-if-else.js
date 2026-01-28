995c58cf427403dafef5dd9a5728c7d3d236d745
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-28 17:14:08 +0530

    nested if else practice

---
+++ jS-practice/nested-if-else.js
@@ -0,0 +1,18 @@
+const money = 99;
+
+if (money >= 400) {
+    console.log("bro your'e rich");
+}
+else {
+    if (money >= 100) {
+        console.log("tui gorib o na borolok o na ");
+    }
+    else {
+        if (money >= 10) {
+            console.log("tui kola kha, calcium bara")
+        }
+        else {
+            console.log("tui amar bondhub na ")
+        }
+    }
+}
